c9b1829e8c2c656c8ea2a517d1f1865095173ec7
Kamshory <kamshory@yahoo.com>
2023-02-09 09:36:35 +0700

    Update sinkronisasi.php

--- admin/sinkronisasi.php
+++ admin/sinkronisasi.php
@@ -6,6 +6,12 @@ if(!empty($school_id))
 {
     require_once dirname(__FILE__) . "/lib.inc/header.php"; //NOSONAR
     ?>
+    <style>
+        .sync-container{
+            margin-bottom: 20px;
+        }
+    </style>
+    <div class="sync-container">
     <h4>Sinkronisasi File</h4>
     <div class="">
         <div>Download informasi dari sync hub</div>
@@ -15,7 +21,28 @@ if(!empty($school_id))
     </div>
 
     <div class="">
-        <div>Download file dari sync hub</div>
+        <div>Download file sync dari sync hub</div>
+        <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
+
+    <div class="">
+        <div>Download file pengguna dari sync hub</div>
+        <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
+
+    <div class="">
+        <div>Upload file pengguna ke sync hub</div>
+        <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
+
+    <div class="">
+        <div>Upload file sync ke sync hub</div>
         <div class="progress">
             <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
@@ -24,24 +51,12 @@ if(!empty($school_id))
     <div class="">
         <div>Upload informasi ke sync hub</div>
         <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-    </div>
-
-    <div class="">
-        <div>Upload file ke sync hub</div>
-        <div class="progress">
             <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
     </div>
 
-    <div class="">
-        <div>Update informasi ke sync hub</div>
-        <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
     </div>
-
+    <div class="sync-container">
 
     <h4>Sinkronisasi Database</h4>
     <div class="">
@@ -52,14 +67,14 @@ if(!empty($school_id))
     </div>
 
     <div class="">
-        <div>Download query dari sync hub</div>
+        <div>Download file sync dari sync hub</div>
         <div class="progress">
             <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
     </div>
 
     <div class="">
-        <div>Eksekusi query dari sync hub</div>
+        <div>Eksekusi query dari file sync</div>
         <div class="progress">
             <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
@@ -73,10 +88,11 @@ if(!empty($school_id))
     </div>
 
     <div class="">
-        <div>Upload query ke sync hub</div>
+        <div>Upload file sync ke sync hub</div>
         <div class="progress">
             <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
+    </div>
     </div>
 
 
