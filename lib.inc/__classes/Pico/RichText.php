482189ab54173651329d5c77bad36b319498063b
Kamshory, MT <kamshory@yahoo.com>
2023-03-26 21:26:04 +0700

    Update RichText.php

--- lib.inc/__classes/Pico/RichText.php
+++ lib.inc/__classes/Pico/RichText.php
@@ -5,64 +5,112 @@ namespace Pico;
 class RichText
 {
 
-	private function isVideo($s)
+	/**
+	 * Check if input is valid video
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isVideo($input)
 	{
-		return stripos($s, "://") !== false
-			&& stripos($s, "facebook.com") !== false
-			&& (stripos($s, "/videos/") !== false || stripos($s, "/video/embed") !== false);
+		return stripos($input, "://") !== false
+			&& stripos($input, "facebook.com") !== false
+			&& (stripos($input, "/videos/") !== false || stripos($input, "/video/embed") !== false);
 	}
 
-	private function isYouTube($s)
+	/**
+	 * Check if input is YouTube URL
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isYouTube($input)
 	{
-		return stripos($s, "://") !== false
-			&& stripos($s, "youtube.com") !== false;
+		return stripos($input, "://") !== false
+			&& stripos($input, "youtube.com") !== false;
 	}
 
-	private function isYouTubeShort($s)
+	/**
+	 * Check if input is ort YouTube URL
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isYouTubeShort($input)
 	{
-		return stripos($s, "://") !== false
-			&& stripos($s, "youtu.be") !== false;
+		return stripos($input, "://") !== false
+			&& stripos($input, "youtu.be") !== false;
 	}
 
-	private function isYtImage($s)
+	/**
+	 * Check if input is YouTube thumbnail image
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isYtImage($input)
 	{
-		return stripos($s, "://") !== false
-			&& stripos($s, "ytimg.com") !== false;
+		return stripos($input, "://") !== false
+			&& stripos($input, "ytimg.com") !== false;
 	}
 
-	private function isContainURL($s)
+	/**
+	 * Check if input is contain URL
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isContainURL($input)
 	{
-		return stripos($s, "://") !== false;
+		return stripos($input, "://") !== false;
 	}
 
-	private function isContainTag($s)
+	/**
+	 * Check if input is containing tag
+	 *
+	 * @param string $input
+	 * @return boolean
+	 */
+	private function isContainTag($input)
 	{
-		return stripos($s, "#") === 0;
+		return stripos($input, "#") === 0;
 	}
 
-	private function createVideo($s)
+	/**
+	 * Create video element
+	 *
+	 * @param string $input
+	 * @return string
+	 */
+	private function createVideo($input)
 	{
-		$souce = htmlspecialchars_decode($s);
-		$start1 = stripos($s, "/video/embed");
-		$start2 = stripos($s, "/videos/");
+		$souce = htmlspecialchars_decode($input);
+		$start1 = stripos($input, "/video/embed");
+		$start2 = stripos($input, "/videos/");
 		if ($start1 !== false) {
 			$data = parse_url($souce);
 			parse_str(@$data['query'], $args);
 			$video_id = @$args['video_id'];
-			$s = '<iframe width="100%" height="218" src="https://www.facebook.com/video/embed?video_id=' . $video_id . '" frameborder="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" scrolling="no" allowfullscreen allowtransparency="true" style="display:block;margin:4px 0px;"></iframe>';
+			$input = '<iframe width="100%" height="218" src="https://www.facebook.com/video/embed?video_id=' . $video_id . '" frameborder="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" scrolling="no" allowfullscreen allowtransparency="true" style="display:block;margin:4px 0px;"></iframe>';
 		}
 		if ($start2 !== false) {
-			$substr = ltrim(substr($s, $start2 + 8), "/");
+			$substr = ltrim(substr($input, $start2 + 8), "/");
 			$arr = explode("/", $substr);
 			$video_id = $arr[0];
-			$s = '<iframe width="100%" height="218" src="https://www.facebook.com/video/embed?video_id=' . $video_id . '" frameborder="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" scrolling="no" allowfullscreen allowtransparency="true" style="display:block;margin:4px 0px;"></iframe>';
+			$input = '<iframe width="100%" height="218" src="https://www.facebook.com/video/embed?video_id=' . $video_id . '" frameborder="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" scrolling="no" allowfullscreen allowtransparency="true" style="display:block;margin:4px 0px;"></iframe>';
 		}
-		return $s;
+		return $input;
 	}
 
-	private function createYouTube($s)
+	/**
+	 * Create YouTube video element
+	 *
+	 * @param string $input
+	 * @return string
+	 */
+	private function createYouTube($input)
 	{
-		$souce = htmlspecialchars_decode($s);
+		$souce = htmlspecialchars_decode($input);
 		$data = parse_url($souce);
 		parse_str(@$data['query'], $args);
 		$vid = @$args['v'];
@@ -80,13 +128,19 @@ class RichText
 		$t = $arr2[0] + (@$arr2[1] * 60) + (@$arr2[2] * 3600);
 		$time = $t;
 		$yurl = "https://www.youtube.com/embed/$vid?html5=1&amp;playsinline=1&amp;allowfullscreen=true&amp;rel=0&amp;version=3&amp;autoplay=1&amp;start=$time"; //NOSONAR
-		$s = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
-		return $s;
+		$input = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
+		return $input;
 	}
 
-	private function createYpuTubeShort($s)
+	/**
+	 * Create short YouTube video element
+	 *
+	 * @param string $input
+	 * @return string
+	 */
+	private function createYouTubeShort($input)
 	{
-		$souce = htmlspecialchars_decode($s);
+		$souce = htmlspecialchars_decode($input);
 		$data = parse_url($souce);
 		$vid = trim(@$data['path'], "/");
 		parse_str(@$data['query'], $args);
@@ -97,32 +151,32 @@ class RichText
 		$t = $arr2[0] + (@$arr2[1] * 60) + (@$arr2[2] * 3600);
 		$time = $t;
 		$yurl = "https://www.youtube.com/embed/$vid?html5=1&amp;playsinline=1&amp;allowfullscreen=true&amp;rel=0&amp;version=3&amp;autoplay=1&amp;start=$time"; //NOSONAR
-		$s = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
-		return $s;
+		$input = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
+		return $input;
 	}
 
-	private function createYtImage($s)
+	private function createYtImage($input)
 	{
-		$souce = htmlspecialchars_decode($s);
+		$souce = htmlspecialchars_decode($input);
 		$data = parse_url($souce);
 		$str = trim(@$data['path'], "/");
 		$arr = explode("/", $str);
 		$vid = @$arr[1];
 		$yurl = "https://www.youtube.com/embed/$vid?html5=1&amp;playsinline=1&amp;allowfullscreen=true&amp;rel=0&amp;version=3&amp;autoplay=1"; //NOSONAR
-		$s = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
-		return $s;
+		$input = '<div class="youtube-video"><a href="javascript:;" data-url="' . $yurl . '"><img data-full-width="true" class="image-youtube-video" src="https://i1.ytimg.com/vi/' . $vid . '/hqdefault.jpg" /><div class="video-player"></div></a></div>'; //NOSONAR
+		return $input;
 	}
 
-	private function createURL($s)
+	private function createURL($input)
 	{
-		$souce = htmlspecialchars_decode($s);
-		$arr = explode("://", $s, 2);
+		$souce = htmlspecialchars_decode($input);
+		$arr = explode("://", $input, 2);
 		$protocol1 = $arr[0];
 		$protocol2 = ltrim(preg_replace("/[^A-Za-z\d]/i", "", $protocol1), "0123456789"); //NOSONAR
 		if ($protocol1 != $protocol2) {
 			$url = $protocol2 . "://" . $arr[1];
 		} else {
-			$url = $s;
+			$url = $input;
 		}
 		$url = filter_var($url, FILTER_VALIDATE_URL);
 		if (substr_count($url, "/") == 3) {
@@ -150,12 +204,12 @@ class RichText
 		return str_replace($link, "<a href=\"" . $link . "\" target=\"_blank\">$link</a>", $souce);
 	}
 
-	private function createTag($s)
+	private function createTag($input)
 	{
-		$souce = htmlspecialchars_decode($s);
+		$souce = htmlspecialchars_decode($input);
 		$numtags = substr_count($souce, "#");
 		if ($numtags == 1) {
-			$s = "<a class=\"link-to-tag post-tag\" data-tag=\"" . substr($souce, 1) . "\" href=\"find.php?tag=" . substr($souce, 1) . "\">$souce</a>"; //NOSONAR
+			$input = "<a class=\"link-to-tag post-tag\" data-tag=\"" . substr($souce, 1) . "\" href=\"find.php?tag=" . substr($souce, 1) . "\">$souce</a>"; //NOSONAR
 		} else {
 			$arr = explode("#", $souce);
 			$arr2 = array();
@@ -163,14 +217,14 @@ class RichText
 				$arr2[$key] = "<a class=\"link-to-tag post-tag\" data-tag=\"" . $val . "\" href=\"find.php?tag=" . $val . "\">#$val</a>"; //NOSONAR
 				$arr[$key] = "#" . $arr[$key];
 			}
-			$s = str_replace($arr, $arr2, $s);
+			$input = str_replace($arr, $arr2, $input);
 		}
-		return $s;
+		return $input;
 	}
 
-	private function createEmailOrUserLink($s)
+	private function createEmailOrUserLink($input)
 	{
-		$souce = htmlspecialchars_decode($s);
+		$souce = htmlspecialchars_decode($input);
 		$val = $souce;
 		$arr3 = preg_split("/[^\@a-zA-Z0-9_]+/", $val);
 		$arr4 = array();
@@ -189,7 +243,7 @@ class RichText
 			$arr5[] = "<a class=\"link-to-user mention-user\" data-username=\"" . substr($val3, 1) . "\" href=\"" . substr($val3, 1) . "\">$val3</a>";
 		}
 
-		$s = str_replace($arr4, $arr5, $s);
+		$input = str_replace($arr4, $arr5, $input);
 
 		if (stripos($val, "#") !== false) {
 			$arr6 = explode("#", $val);
@@ -198,15 +252,15 @@ class RichText
 				$arr7[$key6] = "<a class=\"link-to-tag post-tag\" data-tag=\"" . ($val6) . "\" href=\"find.php?tag=" . ($val6) . "\">#$val6</a>";
 				$arr6[$key6] = "#" . $arr6[$key6];
 			}
-			$s = str_replace($arr6, $arr7, $s);
+			$input = str_replace($arr6, $arr7, $input);
 		}
-		return $s;
+		return $input;
 	}
 
-	private function createLinkURL($s)
+	private function createLinkURL($input)
 	{
-		$souce = htmlspecialchars_decode($s);
-		if (stripos($s, "/") === false) {
+		$souce = htmlspecialchars_decode($input);
+		if (stripos($input, "/") === false) {
 			$link = preg_replace('/^\PL+|\PL\z/', '', $souce);
 		} else {
 			$link = $souce;
@@ -214,36 +268,36 @@ class RichText
 		return str_replace($link, "<a href=\"http://" . $link . "\" target=\"_blank\">$link</a>", $souce);
 	}
 
-	public function createRichContent($s)
+	public function createRichContent($input)
 	{
-		$souce = htmlspecialchars_decode($s);
-		if ($this->isVideo($s)) {
-			$s = $this->createVideo($s);
-		} else if ($this->isYouTube($s)) {
-			$s = $this->createYouTube($s);
-		} else if ($this->isYouTubeShort($s)) {
-			$s = $this->createYpuTubeShort($s);
-		} else if ($this->isYtImage($s)) {
-			$s = $this->createYtImage($s);
-		} else if ($this->isContainURL($s)) {
-			$s = $this->createURL($s);
-		} else if ($this->isContainTag($s)) {
+		$souce = htmlspecialchars_decode($input);
+		if ($this->isVideo($input)) {
+			$input = $this->createVideo($input);
+		} else if ($this->isYouTube($input)) {
+			$input = $this->createYouTube($input);
+		} else if ($this->isYouTubeShort($input)) {
+			$input = $this->createYouTubeShort($input);
+		} else if ($this->isYtImage($input)) {
+			$input = $this->createYtImage($input);
+		} else if ($this->isContainURL($input)) {
+			$input = $this->createURL($input);
+		} else if ($this->isContainTag($input)) {
 			// test
-			$s = $this->createTag($s);
-		} else if (stripos($s, "@") === 0) {
-			$s = $this->createEmailOrUserLink($s);
-		} else if (stripos($s, "www.") === 0) {
-			$s = $this->createLinkURL($s);
-		} else if (filter_var($s, FILTER_VALIDATE_EMAIL)) {
-			$s = "<a href=\"mailto:" . $souce . "\">$souce</a>";
+			$input = $this->createTag($input);
+		} else if (stripos($input, "@") === 0) {
+			$input = $this->createEmailOrUserLink($input);
+		} else if (stripos($input, "www.") === 0) {
+			$input = $this->createLinkURL($input);
+		} else if (filter_var($input, FILTER_VALIDATE_EMAIL)) {
+			$input = "<a href=\"mailto:" . $souce . "\">$souce</a>";
 		}
-		return $s;
+		return $input;
 	}
 	public function removeYouTubeLink($content, $removeReturn = true)
 	{
-		$s = str_replace("\n", "\r\n", $content);
-		$s = str_replace("\r\n\n", "\r\n", $s);
-		$arr1 = explode("\r\n", $s);
+		$input = str_replace("\n", "\r\n", $content);
+		$input = str_replace("\r\n\n", "\r\n", $input);
+		$arr1 = explode("\r\n", $input);
 		foreach ($arr1 as $key1 => $val1) {
 			$arr2 = @preg_split('/\s+/', $val1);
 			foreach ($arr2 as $key2 => $val2) {
